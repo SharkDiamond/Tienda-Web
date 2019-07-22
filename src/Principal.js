0a1d05a40b70e67df29ac71566a00c07dda741ac
gabriel <gabrielarispe24@gmail.com>
2019-07-22 12:23:14 -0400

    Se le agregaron a las cartas del principal imagen y titulo

--- src/Principal.js
+++ src/Principal.js
@@ -40,7 +40,7 @@ this.state={};
 
 <Mensaje texto="Bienvenido A Lutius Tu Tienda De Confianza"/>
 
-<GrupoCartas  titulo="Ofertas"/>
+<GrupoCartas  titulo="Ofertas" imagencarta1="https://cdn4.cnet.com/img/kONg4IUDeq5EEHwczPhZonIWDo0=/2018/03/23/c44fa164-d20c-4640-a609-e8ebe7e22b3a/huawei-p20-pro-hero-promo-4.jpg " imagencarta2="https://assets.pcmag.com/media/images/547224-samsung-galaxy-smartphones.jpg?width=810&height=456" imagencarta3="https://cdn3.cnet.com/img/HvuNEudjVWeEVzsvE5JkUpqYMpM=/868x488/2019/05/01/d3441f9c-2ab5-4e51-920e-8196cc565623/honor-10-lite-analisis.jpg " titulocarta1="Huawei p20 Pro" titulocarta2="Samsumg Galaxy S10" titulocarta3="Honor" />
 
 <div className="m-5"><Slider/></div>
 <PiePagina/>
