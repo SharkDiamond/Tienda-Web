080e3ed53ba3f69dbbd2775f18a14c517eab5c11
gabriel <gabrielarispe24@gmail.com>
2019-07-21 23:05:03 -0400

    Se agrego diseño responsivo al elemento grupo de cartas y animacion de color naranja

---
+++ src/Carta.js
@@ -0,0 +1,86 @@
+import React from 'react';
+import './Barra.css';
+import { Button } from "reactstrap";
+
+
+
+class Carta extends React.Component {
+
+
+constructor(){
+
+super();
+
+this.state={};
+
+this.color6=this.color6.bind(this);
+this.color7=this.color7.bind(this);
+
+
+
+this.color8=this.color8.bind(this);
+this.color9=this.color9.bind(this);
+
+
+}
+
+
+
+color6(e){
+    e.target.className="card-title text-center  letra h5 color_df1";
+    
+    }
+    
+    color7(e){
+      e.target.className="card-title text-center  letra h5";
+    
+    }
+    
+
+    color8(e){
+        e.target.className="card-text texto text-dark letra text-center color_df1";
+        
+        }
+        
+        color9(e){
+          e.target.className="card-text texto text-dark letra text-center";
+        
+        }
+
+
+
+
+  render() {
+
+  
+const carta=(
+<div className="card m-1  border-dark rounded color_df">
+<img src={this.props.imagenCarta} class="card-img-top" alt="..."  height="200px" width="200px"/>
+<div className="card-body border text-center color_df">
+<div className="card-title text-center  letra h5" onMouseMove={this.color6} onMouseOut={this.color7}>{this.props.tituloCarta}</div>
+<div className="card-text texto text-dark letra text-center"   onMouseMove={this.color8} onMouseOut={this.color9}>Antes <strike>{this.props.precioanteriorcarta}</strike>$ ahora {this.props.precio}$</div>
+<br></br>
+<button className="btn btn bg-success letra text-white">Hechar un vistazo</button>
+</div>
+</div>
+  
+  );
+ 
+
+
+
+  return(
+<div className="">
+
+    {carta}
+
+    </div>
+  );
+
+
+
+
+}
+
+}
+export default  Carta;
--- src/GrupoCartas.js
+++ src/GrupoCartas.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import './Barra.css';
 import { Button } from "reactstrap";
+import  Carta  from './Carta.js';
+
+
+
+
+
+
 class GrupoCartas extends React.Component {
 
 
@@ -10,123 +17,66 @@ super();
 
 this.state={};
 
+this.color1=this.color1.bind(this);
+this.color2=this.color2.bind(this);
+}
 
+
+color1(e){
+e.target.className="border rounded border-dark color_df1 display-2 text-center  letra_texto";
+
+}
+
+color2(e){
+  e.target.className=" display-2 text-center  letra_texto";
 
 }
 
 
-
-
-  render() {
-
-    const carta=(
-
-<div className="card tamaño_cartas m-3 ">
-    <img src={this.props.imagenCarta1} class="card-img-top border border-dark rounded-top" width="3000px" height="188px" alt="..."/>
-    <div className="card-body text-center border-left border-right border-bottom border-dark  rounded-bottom">
-      <h5 className="card-title text-center letra">{this.props.tituloCarta1}</h5>
-      <p className="card-text text-center letra"><strike>{this.props.precioanteriocarta1}</strike> ahora 400$</p>
-     <Button color="success" ><span className="letra">Echar Un Vistazo</span></Button>
-    </div>
-  </div>
-
-
-
-    );
+render() {
     
-
-    const carta2=(
-
-      <div className="card tamaño_cartas m-3 ">
-          <img src={this.props.imagenCarta2} class="card-img-top border border-dark rounded-top" width="10px" height="200px" alt="..."/>
-          <div className="card-body text-center border-left border-right border-bottom border-dark  rounded-bottom">
-            <h5 className="card-title text-center letra">{this.props.tituloCarta2}</h5>
-            <p className="card-text text-center letra"><strike>{this.props.precioanteriocarta2}</strike> ahora 90$</p>
-           <Button color="success" ><span className="letra">Echar Un Vistazo</span></Button>
-          </div>
-        </div>
-      
-      
-      
-          );
-
-
-          const carta3=(
-
-            <div className="card tamaño_cartas m-3 ">
-                <img src={this.props.imagenCarta3} class="card-img-top border border-dark rounded-top" width="10px" height="200px" alt="..."/>
-                <div className="card-body text-center border-left border-right border-bottom border-dark  rounded-bottom">
-                  <h5 className="card-title text-center letra">{this.props.tituloCarta3}</h5>
-                  <p className="card-text text-center letra"><strike>{this.props.precioanteriocarta3}</strike> ahora 490$</p>
-                 <Button color="success" ><span className="letra">Echar Un Vistazo</span></Button>
-                </div>
-              </div>
-            
-            
-            
-                );
-
-
-
   return(
-<div className="fondo_mensaje border rounded  border-dark m-1">
+<div className="color_df" >
 
 
-<br></br>
-<h1 className="text-center letra_texto display-4">{this.props.titulo}</h1>
-<div className="container">
+<h1 className="display-2 text-center  letra_texto" onMouseMove={this.color1} onMouseOut={this.color2}>Ofertas</h1>
+
+<div className="container m-1">
+
 <div className="row">
 
 <div className="cold-4">
 
-{carta}
-
-</div>
-
-
-<div className="cold-4">
-
-{carta2}
+<Carta imagenCarta="https://cdn2.cnet.com/img/vHyu3R0p_9WoRK327bCMs58xBPU=/1600x900/2018/12/04/0e6c5e54-4454-442d-b7af-f508a8df3026/samsung-s9-3.jpg" tituloCarta="P20 Pro"  precio="200" precioanteriorcarta="600"/>
 
 </div>
 
 <div className="cold-4">
 
-{carta3}
+
+<Carta imagenCarta="https://cdn2.cnet.com/img/vHyu3R0p_9WoRK327bCMs58xBPU=/1600x900/2018/12/04/0e6c5e54-4454-442d-b7af-f508a8df3026/samsung-s9-3.jpg" tituloCarta="P20 Pro" precio="200" precioanteriorcarta="600"/>
+
+</div>
+
+<div className="cold-4">
+
+<Carta imagenCarta="https://cdn2.cnet.com/img/vHyu3R0p_9WoRK327bCMs58xBPU=/1600x900/2018/12/04/0e6c5e54-4454-442d-b7af-f508a8df3026/samsung-s9-3.jpg" tituloCarta="P20 Pro"  precio="200"  precioanteriorcarta="600"/>
+
+</div>
+
+
 
 </div>
 
 
 </div>
-
+<br></br>
 </div>
 
-
-
-
-<br></br>
-<br></br>
-<br></br>
-<br></br>
-<br></br>
-<br></br>
-<br></br>
-<br></br>
-<br></br>
-<br></br>
-<br></br>
-<br></br>
-<br></br>
-<br></br>
-<br></br>
-
-    </div>
-  );
-
-
-
+);
 
 }
-
 }
+
+
 export default  GrupoCartas;
