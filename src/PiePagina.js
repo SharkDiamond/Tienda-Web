90653c900168073f18fa72e64b04ee07d54aa62d
gabriel <gabrielarispe24@gmail.com>
2019-07-28 23:08:51 -0400

    Se mejoro la estructura de footer haciendo que las letras sean responsive sin embargo falta mejorarlo agregandole mas cosas como los metodos de pagos

--- src/PiePagina.js
+++ src/PiePagina.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Barra.css';
-
+import './pie.css';
 class PiePagina extends React.Component {
 
 
@@ -25,28 +25,28 @@ this.state={};
 <div className="container-fluid bg-white">
 
 
-<p className="display-1 text-center letra_texto">Tu Tienda De Confianza</p>
-<p className="display-4 text-center letra_texto">Redes Sociales</p>
+<p className="tama  text-center letra_texto">Tu Tienda De Confianza</p>
+<p className="tama2 text-center letra_texto">Redes Sociales</p>
 
 
 
 
 <div className="row justify-content-center">
 
-<div className="d-inline d-sm-block m-1">
+<div className="d-inline m-1">
 <a><img src="https://image.flaticon.com/icons/svg/33/33702.svg" width="50px" height="50px"/> Facebook</a>
 
 
 </div>
 
-<div className="d-inline d-sm-block m-1">
+<div className="d-inline  m-1">
 <a><img src="https://image.flaticon.com/icons/svg/1051/1051313.svg" width="50px" height="50px"/> Instagram</a>
 
 
 
 </div>
 
-<div className="d-inline m-1">
+<div className="d-inline  m-1">
 <a><img src="https://image.flaticon.com/icons/svg/1051/1051331.svg" width="50px" height="50px"/> Twitter</a>
 
 </div>
@@ -54,12 +54,12 @@ this.state={};
 </div>
 
 
-
+<br></br>
 
 <div className="row justify-content-center">
 
 
-<p className="letra h4">Todos Los Derechos Reservados</p>
+<p className="letra h4 text-center">Todos Los Derechos Reservados</p>
 
 </div>
     </div>
