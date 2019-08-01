81c0343d70f1b6d49a664817c5dd0e4db9c072de
GabrielOficina <gabrielarispe24@gmail.com>
2019-08-01 14:53:25 -0400

    Se empezo a trabajar en el componente telefono

--- src/Vinculos/Telefonos.js
+++ src/Vinculos/Telefonos.js
@@ -1,2 +1,60 @@
-COMPONENTE TELEFONOS.
+
+import React from 'react';
+import './App.css';
+import  Barra  from './Barra.js';
+import  Mensaje  from './Mensaje.js';
+import  GrupoCartas  from './GrupoCartas.js';
+import Slider  from './Slider.js';
+import  PiePagina  from './PiePagina.js';
+import BarraResponsive from './BarraResponsive';
+
+
+
+
+
+class Telefonos extends React.Component {
+
+
+constructor(){
+
+super();
+
+this.state={};
+
+
+
+}
+
+
+
+
+  render() {
+
+
+
+  return(
+<div className="">
+
+<BarraResponsive/>
+<Barra/>
+
+<div className="container justify-content-center">
+
+
+
+
+
+
+</div>
+<PiePagina/>
+
+    </div>
+  );
+
+
+}
+
+}
+export default  Telefonos;
+
 
